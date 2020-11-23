f4e016399d3065d5bd619ed49cf832c5bf87ad8d
tryoma <tryoma0326@gmail.com>
2020-11-23 20:31:14 +0900

    modify_application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'main_pages/top'
+  root 'main_pages#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
