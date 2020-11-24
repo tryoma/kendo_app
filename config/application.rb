dcaed06822f31569557ad4a68eef870495de62e1
tryoma <tryoma0326@gmail.com>
2020-11-24 20:04:23 +0900

    install_devise

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module KendoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo' 
 
     # Settings in config/environments/* take precedence over those specified here.
