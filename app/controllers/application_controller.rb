80f68546c82bd119284edba4515f3d0c28493b21
tryoma <tryoma0326@gmail.com>
2020-11-24 20:48:14 +0900

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # ログイン済ユーザーのみにアクセスを許可する
+  before_action :authenticate_user!
+
+  # deviseコントローラーにストロングパラメータを追加する          
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # サインイン後のリダイレクト先をマイページへ
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "ログインに成功しました" 
+    root_path  
+  end
+
+  # サインアウト後のリダイレクト先をトップページへ
+  def after_sign_out_path_for(resource)
+    flash[:alert] = "ログアウトしました"
+    root_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    # サインアップ時にnameのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    # アカウント編集の時にnameとprofileのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name])
+  end
 end
--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,4 +1,5 @@
 class MainPagesController < ApplicationController
   def top
+    debugger
   end
 end
