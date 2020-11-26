9b29f4b3c3b5c944f34213bf61be73a4645d6c83
tryoma <tryoma0326@gmail.com>
2020-11-26 20:58:32 +0900

    Finish add-events

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :users
+  resources :events
   root 'main_pages#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
