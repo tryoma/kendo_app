f61f7c0a6baa273314c96a03e1adcf66065b6b9f
tryoma <tryoma0326@gmail.com>
2020-12-13 10:50:05 +0900

    modify_seed

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -3,5 +3,6 @@ class MainPagesController < ApplicationController
   def top
     @events = Event.all
     @informations = Information.all
+    @users = User.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,27 @@ User.create!(user_name: "Sample User1",
   password: "password",
   password_confirmation: "password",
   admin: false)
+
+4.times do
+  date  = Faker::Date.between(from: '2020-12-25', to: '2021-12-25')
+  Event.create!(event_day: date,
+                start_time: "11:00",
+                finish_time: "12:00",
+                prefecture: "千葉県",
+                place: "ディズニーランド",
+                estimate_people: "5",
+                level: "10",
+                comment: "ミッキーと一緒に稽古しよう！",
+                user_id: 2 )
+end
+
+4.times do |n|
+  date  = Faker::Date.between(from: '2020-12-25', to: '2021-12-25')
+  title = "お役たち情報#{n}" 
+  body = "#{n}号店オープン！"
+  date2  = Faker::Date.between(from: '2020-12-25', to: '2021-12-25')
+  Information.create!(title: title,
+                      body: body,
+                      until: date2,
+                      user_id: 1 )
+end
