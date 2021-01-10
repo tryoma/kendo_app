6d6138ff8280675e658ee916c817ee6023074d0a
tryoma <tryoma0326@gmail.com>
2021-01-10 09:26:43 +0900

    modifi

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   resources :events do
     resources :members, :only => [:create, :update, :destroy]
-    resources :comments, :only => [:create, :destroy]
+    resources :comments, :only => [:create, :destroy, :index]
   end
   
   resources :informations do
