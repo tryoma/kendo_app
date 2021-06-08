fd94565e7782d3e1f3433f4d165683b56710e524
tryoma <tryoma0326@gmail.com>
2021-06-08 15:34:25 +0900

    editing_line

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -87,7 +87,7 @@ class EventsController < ApplicationController
         "type": "bubble",
         "hero": {
           "type": "image",
-          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+          "url": "/images/kendo.png",
           "size": "full",
           "aspectRatio": "20:13",
           "aspectMode": "cover"
