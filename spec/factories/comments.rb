df2e435b70551f2ff4cd9ca1925002ff1dbf27e3
tryoma <tryoma0326@gmail.com>
2021-06-10 16:06:38 +0900

    create_user_spec

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    body "この会場は飲食可能でしょうか？"
+    body { 'この会場は飲食可能でしょうか？' }
     association :user
     association :event
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :user, aliases: [:owner, :follower, :following] do
-    user_name "大城戸功"
+    user_name { '大城戸功' }
     sequence(:email) { |n| "tester#{n}@example.com" }
-    password "password"
+    admin { false }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
