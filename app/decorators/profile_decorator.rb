b39524f8d677ac4cd8fc8ff6f1d2b2eefbff3133
tryoma <tryoma0326@gmail.com>
2021-06-15 11:39:41 +0900

    update_gem

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -1,5 +1,5 @@
 module ProfileDecorator
   def photo_image
-    self.photo.attached? ? self.photo.variant(combine_options:{gravity: :center, resize:"640x640^",crop:"640x640+0+0"}).processed : "default.png"
+    photo.attached? ? photo.variant(gravity: :center, resize: "640x640^", crop: "640x640+0+0").processed : "default.png"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_06_085312) do
+ActiveRecord::Schema.define(version: 2021_06_15_021332) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -27,10 +27,17 @@ ActiveRecord::Schema.define(version: 2021_06_06_085312) do
     t.string "filename", null: false
     t.string "content_type"
     t.text "metadata"
-    t.bigint "byte_size", null: false
+    t.integer "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
@@ -53,7 +60,7 @@ ActiveRecord::Schema.define(version: 2021_06_06_085312) do
     t.integer "estimate_people"
     t.integer "level", default: 0, null: false
     t.string "comment"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_events_on_user_id"
@@ -72,7 +79,7 @@ ActiveRecord::Schema.define(version: 2021_06_06_085312) do
     t.date "limited"
     t.string "youtube"
     t.string "youtube_url"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_information_on_user_id"
@@ -86,8 +93,8 @@ ActiveRecord::Schema.define(version: 2021_06_06_085312) do
   end
 
   create_table "members", force: :cascade do |t|
-    t.integer "event_id"
-    t.integer "user_id"
+    t.bigint "event_id"
+    t.bigint "user_id"
     t.boolean "first_agree"
     t.boolean "second_agree"
     t.datetime "created_at", precision: 6, null: false
@@ -114,7 +121,7 @@ ActiveRecord::Schema.define(version: 2021_06_06_085312) do
     t.string "title"
     t.text "content"
     t.boolean "practice", default: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_records_on_user_id"
@@ -142,6 +149,7 @@ ActiveRecord::Schema.define(version: 2021_06_06_085312) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "events", "users"
   add_foreign_key "information", "users"
   add_foreign_key "members", "events"
